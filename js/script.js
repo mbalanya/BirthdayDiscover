7dae99790b393dc32f6a887c203be9b2e6209b99
mbalanya <okobacharles@yahoo.co.uk>
2020-05-08 14:13:28 +0300

    add dropdown list elements for birthday date in script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-var elements = ""
+var elementsy = ""
+var elementsm = ""
+var elementsd = ""
 var year;
+var month;
+var day;
 for(year= 1900; year < 2021; year++){
-    elements += "<option value='"+ year + "'>" + year + "</option>";
+    elementsy += "<option value='"+ year + "'>" + year + "</option>";
 }
+document.getElementById("selectYear").innerHTML = elementsy;
 
+for(month= 1; month < 13; month++){
+    elementsm += "<option value='"+ month + "'>" + month + "</option>";
+}
+document.getElementById("selectMonth").innerHTML = elementsm;
 
-document.getElementById("selBox").innerHTML = elements;
+for(day= 1; day < 32; day++){
+    elementsd += "<option value='"+ day + "'>" + day + "</option>";
+}
+document.getElementById("selectDay").innerHTML = elementsd;
 
 mname = ['Kwame','Kwasi', 'Kwadwo', 'Kwebena', 'Kwaku', 'Yaw', 'Kofi'];
 fname = ['Ama', 'Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua'];
